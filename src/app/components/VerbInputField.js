416a071c32012d33a89c92700d232c9a40e10aeb
MiejkeHoltermann <miejke.holt@gmx.de>
2024-01-07 15:23:20 +0100

    improve verb function

--- src/app/components/VerbInputField.js
+++ src/app/components/VerbInputField.js
@@ -6,7 +6,7 @@ export default function VerbInputField({
   isCorrect,
 }) {
   return (
-    <div key={i} className="flex items-center gap-4">
+    <div key={i} className="flex items-center justify-end gap-4">
       <label htmlFor={`presente0${i + 1}`}>{label}</label>
       <input
         type="text"
@@ -18,10 +18,10 @@ export default function VerbInputField({
           newValues[i] = e.target.value;
           setPresenteValues(newValues);
         }}
-        className={`border rounded-lg p-1 ${
+        className={`w-[70%] flex flex-col rounded-xl px-4 py-2 mr-5 focus:outline-none border ${
           isCorrect[i] === false
-            ? "border-2 border-red-500 text-red-700"
-            : "border-1 border-gray-400"
+            ? "border-2 border-red-500 text-red-700 shadow-none"
+            : "border-1 shadow-[2px_2px_rgba(0,215,177,1)]"
         }`}
       />
     </div>
