b7517e07de151f25c9e1f10cbf3aa6ff7fd05174
MiejkeHoltermann <miejke.holt@gmx.de>
2024-03-16 16:32:28 +0100

    simplify verbExists API

--- src/app/api/users/[userId]/words/route.js
+++ src/app/api/users/[userId]/words/route.js
@@ -86,7 +86,7 @@ export async function DELETE(request) {
 }
 
 export async function POST(request) {
-  const { userId, germanWord, italianWord } = await request.json();
+  const { userId, germanWord, italianWord, name } = await request.json();
 
   await connectMongoDB();
   try {
@@ -94,15 +94,18 @@ export async function POST(request) {
     if (!user) {
       return res.status(404).json({ message: "User not found" });
     }
-
-    const word1 = await user.customWords.find(
-      (word) => word.germanWord === germanWord
-    );
-    const word2 = await user.customWords.find(
-      (word) => word.italianWord === italianWord
-    );
-
-    return NextResponse.json({ word1, word2 });
+    if (germanWord && italianWord) {
+      const word1 = await user.customWords.find(
+        (word) => word.germanWord === germanWord
+      );
+      const word2 = await user.customWords.find(
+        (word) => word.italianWord === italianWord
+      );
+      return NextResponse.json({ word1, word2 });
+    } else {
+      const verb = await user.customVerbs.find((verb) => verb.name === name);
+    }
+    return NextResponse.json({ verb });
   } catch (error) {
     console.log(error);
   }
